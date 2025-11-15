a0748fcebdf8c0edbe328e3c7ad19258b0691bfe
= <xavierdeoliveiramauricio@gmail.com>
2025-11-15 03:31:25 -0300

    feat: initial commit

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config.js";
+import "dotenv-expand/config.js";
+
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
